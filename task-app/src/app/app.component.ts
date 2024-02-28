506f3d983187fa193fe9e1b4e4c06a9855ec3d50
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-28 23:57:07 +0530

    updates: resetting code

--- task-app/src/app/app.component.ts
+++ task-app/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TaskService]
 })
 export class AppComponent {
-  navigationSelected: string = 'home';
-
-  onNavigateSection(arg: string) {
-    this.navigationSelected = arg;
-  }
+  
 }
--- task-app/src/app/app.module.ts
+++ task-app/src/app/app.module.ts
@@ -2,32 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { TasksListComponent } from './components/tasks-list/tasks-list.component';
-import { EditTaskComponent } from './components/tasks/edit-task/edit-task.component';
-import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
-const appRoutes: Routes = [
-  {
-    path: 'update/:id',
-    component: EditTaskComponent
-  }
-]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    TasksComponent,
-    TasksListComponent,
-    EditTaskComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
